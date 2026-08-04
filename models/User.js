fa9dcb4de82e20246bf431cad88e66038a531429
Akash Das <akashdas02052@gmail.com>
2026-08-04 22:47:58 +0530

    Update user model

--- models/User.js
+++ models/User.js
@@ -1,32 +1,44 @@
 const mongoose = require("mongoose")
-const bcrypt = require("bcrypt")
 
-const UserSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
-    name: {
+    phoneNumber: {
       type: String,
-      trim: true,
-      required: true,
+      unique: false,
+      required: [true, "Phone number is required."],
     },
+    phoneSuffix: {
+      type: String,
+      unique: false,
+      required: [true, "Country code is required."],
+    },
+    username: { type: String, required: [true, "Username is required."] },
     email: {
       type: String,
-      required: true,
-      unique: true,
-      trim: true,
       lowercase: true,
+      trim: true,
+      unique: [true, "This email is already exists."],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address.",
+      ],
+      required: [true, "Email is required."],
     },
     password: {
       type: String,
-      require: true,
+      required: [true, "Password is required."],
     },
+    emailOtp: { type: Number },
+    emailOtpExpiry: { type: Date },
+    profilePicture: { type: String },
+    about: { type: String },
+    lastSeen: { type: Date },
+    isOnline: { type: Boolean, default: false },
+    isVerified: { type: Boolean, default: false },
+    agreed: { type: Boolean, default: false },
   },
   { timestamps: true },
 )
 
-UserSchema.methods.comparePassword = async function (password) {
-  return bcrypt.compare(password, this.password) // this.password is the hashed password stored in the database
-}
-
-const User = mongoose.model("User", UserSchema)
-
+const User = mongoose.model("User", userSchema)
 module.exports = User
