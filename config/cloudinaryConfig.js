a556d8b02471deccf578327a825f64842a913cc8
Akash Das <akashdas02052@gmail.com>
2026-08-12 23:18:50 +0530

    Write cloudinary SDK in cloudinary config file

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,40 @@
+const multer = require("multer")
+const cloudinary = require("cloudinary").v2
+require("dotenv").config()
+const fs = require("fs") // Needed to clean up temp files
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+// Upload function
+const uploadFileToCloudinary = (file) => {
+  const options = {
+    resource_type: file.mimetype.startsWith("video") ? "video" : "image",
+  }
+
+  return new Promise((resolve, reject) => {
+    const uploader = file.mimetype.startsWith("video")
+      ? cloudinary.uploader.upload_large
+      : cloudinary.uploader.upload
+
+    uploader(file.path, options, (error, result) => {
+      fs.unlink(file.path, () => {})
+      if (error) {
+        return reject(error)
+      }
+      resolve(result)
+    })
+  })
+}
+
+// Temporarily save uploaded files inside the uploads/ directory. 
+// Expect exactly one uploaded file, and its field name must be media.
+const multerMiddleware = multer({ dest: "uploads/" }).single("media")
+
+module.exports = {
+  multerMiddleware,
+  uploadFileToCloudinary,
+}
